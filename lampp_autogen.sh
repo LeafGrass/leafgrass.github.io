fa21736a38c91f073d93e30ba5585a52e72be397
Librae <librae8226@gmail.com>
2013-07-17 14:48:05 +0800

    build: auto generating to lampp if updated

--- lampp_autogen.sh
+++ lampp_autogen.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-jekyll build --watch --destination /opt/lampp/htdocs/leafgrass > .autogen.log
+stat `pwd` | grep Modify > .autogen.diff.1
+
+while true
+do
+	stat `pwd` | grep Modify > .autogen.diff.2
+	diff .autogen.diff.2 .autogen.diff.1 > /dev/null
+	if [ $? -eq 1 ]; then
+		cp .autogen.diff.2 .autogen.diff.1
+		echo "Update detected, ready to build..."
+		jekyll build --destination /opt/lampp/htdocs/leafgrass
+	fi
+	sleep 5
+done
